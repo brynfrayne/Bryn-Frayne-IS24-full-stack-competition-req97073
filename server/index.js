1a2d229af742e69b0e69b981cb4f2a41b7c6822f
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2023-03-27 15:20:35 -0700

    added the basic logic required for swagger ui

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
 
 app.get('/', (_req, res) => {
@@ -6,6 +8,40 @@ app.get('/', (_req, res) => {
 });
 
 const PORT = 8000;
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "LogRocket Express API with Swagger",
+        version: "0.1.0",
+        description:
+          "This is a simple CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "LogRocket",
+          url: "https://logrocket.com",
+          email: "info@email.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:8000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+  );
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
