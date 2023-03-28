68228110877614118138d41a5683c864dc2f9265
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2023-03-28 16:55:38 -0700

    took 10 random scrum masters and divided the projects amongst them, as well as the dev teams

--- server/data/jsonMerge.js
+++ server/data/jsonMerge.js
@@ -1,17 +1,73 @@
 const fs = require('fs');
 
-// Read the JSON files
-const productsFile = fs.readFileSync('server/data/product_list.json')
-const namesFile = fs.readFileSync('server/data/names.json');
-
-// Parse the JSON data
-const products = JSON.parse(productsFile);
-const names = JSON.parse(namesFile);
-
-// Merge the data
-for (let i = 0; i < products.length; i++) {
-  products[i].Developers = names[i].developers;
+// Helper function to read JSON files
+function readJSONFile(filePath) {
+  const file = fs.readFileSync(filePath);
+  return JSON.parse(file);
 }
 
-// Write the merged data to a new file
-fs.writeFileSync('mergedData.json', JSON.stringify(products));
+// Helper function to merge data
+function mergeData(products, names) {
+  for (let i = 0; i < products.length; i++) {
+    products[i].Developers = names[i].developers;
+  }
+  return products;
+}
+
+// Helper function to write JSON files
+function writeJSONFile(filePath, data) {
+  fs.writeFileSync(filePath, JSON.stringify(data));
+}
+
+// Read the JSON files and merge the data
+const products = readJSONFile('server/data/product_list.json');
+const names = readJSONFile('server/data/names.json');
+const mergedData = mergeData(products, names);
+writeJSONFile('server/data/mergedData.json', mergedData);
+
+// Create a list of 10 scrum masters
+function createScrumMasterList(file) {
+  const parsedFile = readJSONFile(file);
+  const scrumMasterList = parsedFile.map(item => item.scrumMasterName);
+  return scrumMasterList.slice(0, 10);
+}
+
+const tenScrumMasters = createScrumMasterList('./server/data/mergedData.json');
+
+// Assign a scrum master to each product
+function assignScrumMaster(file) {
+  const parsedFile = readJSONFile(file);
+  for (let i = 0; i < parsedFile.length; i++) {
+    parsedFile[i].scrumMasterName = tenScrumMasters[i % 10];
+  }
+  return parsedFile;
+}
+
+const assignedScrumMasters = assignScrumMaster('./server/data/mergedData.json');
+writeJSONFile('./server/data/mergedData.json', assignedScrumMasters);
+
+// Create a list of 10 developers
+function createDevList(file) {
+  const parsedFile = readJSONFile(file);
+  const devList = [];
+  for (let i = 0; i < parsedFile.length; i++) {
+    devList.push(parsedFile[i].Developers);
+  }
+  return devList;
+}
+
+const devList = createDevList('./server/data/mergedData.json');
+const tenDevs = devList.slice(0, 10);
+console.log(tenDevs);
+
+// Assign developers to each product
+function assignDevs(file) {
+  const parsedFile = readJSONFile(file);
+  for (let i = 0; i < parsedFile.length; i++) {
+    parsedFile[i].Developers = tenDevs[i % 10];
+  }
+  return parsedFile;
+}
+
+const assignedDevs = assignDevs('./server/data/mergedData.json');
+writeJSONFile('./server/data/mergedData.json', assignedDevs);
